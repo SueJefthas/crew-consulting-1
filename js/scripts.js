74afc86cfe9ab4914577b035d3fc85ef6be6d300
Sue Jefthas <sueadriejefthas@gmail.com>
2016-04-07 21:30:27 +0200

    Added files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     $("#exploratory-btn").click(function(){
         $("#global-wrapper").slideUp("slow");
 		$("#pocket-wrapper").slideUp("slow");
@@ -22,4 +22,30 @@ $(document).ready(function(){
 		$("#pocket-wrapper").toggle("slow");
 		
     });
+});*/
+
+//SMOOTH SCROLLING
+$(document).ready(function() {
+	$(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+		event.preventDefault;
+		var hash = this.hash;
+		
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top
+		}, 900, function(){
+			window.location.hash = hash;
+		});
+	});
+});
+
+//SLIDE IN ANIMATION
+$(window).scroll(function() {
+	$(".slideanim").each(function(){
+		var pos = $(this).offset().top;
+		
+		var winTop = $(window).scrollTop();
+		if(pos < winTop + 600) {
+			$(this).addClass("slide");
+		}
+	});
 });
